78d0442b41bad4531735c8711e84f3d55b23f21d
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-09-09 22:15:12 +0200

    Tweaks to the parallax

--- frontend/app/components/homepage/Staff.tsx
+++ frontend/app/components/homepage/Staff.tsx
@@ -10,32 +10,42 @@ import { useRef } from "react";
 
 export default function Staff() {
   const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({ target: targetRef });
-  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start center", "end center"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], [0, -300]);
 
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     console.log("Page scroll Y progress: ", latest);
   });
 
   return (
-    <section className="flex h-screen flex-col items-start justify-center gap-8 overflow-hidden bg-white px-8 py-16 lg:flex-row lg:py-52">
-      <div className="relative min-h-full w-full bg-red-400">
+    <section
+      ref={targetRef}
+      className="flex h-screen flex-col items-center justify-center gap-8 overflow-hidden bg-white px-8 py-16 lg:flex-row lg:py-52"
+    >
+      <div className="relative min-h-full w-full">
         <motion.img
-          className="absolute left-1/2 top-1/2 w-fit origin-center border-4 border-black"
+          className="absolute left-1/2 top-1/2 w-fit origin-center"
           style={{ transform: "translate(-50%, -50%)" }}
           src="/images/staff-1.png"
           alt="Staff"
-          ref={targetRef}
         />
         <motion.img
-          className="absolute left-1/2 top-1/2"
-          style={{ transform: "translate(-50%, 0)" }}
+          className="w-1/4"
           src="/images/staff-2.png"
           alt="Staff"
-          style={{ y }}
+          style={{
+            y,
+            transform: "translate(-50%, -50%)",
+            position: "absolute",
+            top: "0",
+            right: "5%",
+          }}
         />
       </div>
-      <div className="w-full bg-green-400">
+      <div className="flex w-full flex-col gap-6">
         <h2 className="font-display text-2xl text-slate-800 md:text-6xl lg:w-3/4 2xl:text-7xl">
           Wykwalifikowana kadra
         </h2>
