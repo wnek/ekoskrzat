b266b31e22b03ce23921beb8a070def2f01d251b
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-09-14 23:23:02 +0200

    Right align the second section

--- frontend/app/components/homepage/Hero.tsx
+++ frontend/app/components/homepage/Hero.tsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
     <div className="top-[5rem]">
-      <section className="flex w-full flex-col bg-stone-100 text-center text-lg lg:flex-row lg:text-left">
+      <section className="grid flex-col bg-stone-100 text-center text-lg lg:grid-cols-2 lg:flex-row lg:text-left">
         <div className="top-[5rem] flex h-[80vh] flex-col items-center justify-center gap-8 px-4 lg:sticky lg:h-[calc(100vh-5rem)] lg:items-start lg:px-16 2xl:px-48">
-          <h2 className="max-w-prose font-display text-4xl text-slate-800 md:text-6xl 2xl:text-8xl">
+          <h2 className="font-display text-4xl text-slate-800 md:text-6xl 2xl:text-7xl">
             <strong>Przestrzeń</strong> dla rozwoju Twojego dziecka
           </h2>
-          <p className="text-pretty text-slate-600 2xl:w-[60ch] 2xl:text-2xl">
+          <p className="text-pretty text-slate-600 2xl:max-w-prose 2xl:text-2xl">
             Ekoskrzat to przedszkole w domu z wielkim ogrodem w cichym zakątku
             dzielnicy Bieżanów - Prokocim.
           </p>
@@ -17,7 +17,7 @@ export default function Hero() {
             {/* <a href="/o-nas">Dowiedz się więcej</a> */}
           </div>
         </div>
-        <div className="w-full">
+        <div className="">
           <div className="h-[calc(100vh-5rem)]">
             <img
               className="h-full w-full object-cover"
--- frontend/app/components/homepage/SecondFeature.tsx
+++ frontend/app/components/homepage/SecondFeature.tsx
@@ -1,15 +1,16 @@
 export default function SecondFeature() {
   return (
-    <section className="my-4 h-[100vh] rounded-3xl lg:h-screen">
+    <section className="my-4 h-[100vh] justify-end rounded-3xl lg:flex lg:h-screen">
       <div
         className="mx-4 h-1/2 rounded-3xl lg:absolute lg:h-full lg:w-[calc(100vw-3rem)]"
         style={{
-          backgroundImage: "url(/images/feature-1.png)",
+          backgroundImage: "url(/images/feature-2.jpg)",
           backgroundSize: "cover",
           backgroundPosition: "center center",
         }}
       ></div>
-      <div className="mx-4 py-4 lg:sticky lg:top-[5rem] lg:mx-8 lg:w-1/3">
+
+      <div className="mx-4 h-fit py-4 lg:sticky lg:top-[5rem] lg:mx-8 lg:w-1/3">
         <div className="flex flex-col gap-8 rounded-3xl bg-stone-100 px-12 py-16 lg:rounded-2xl">
           <h3 className="text-sla font-display text-2xl text-slate-800 2xl:text-4xl">
             <strong>Unikalne</strong> podejście do dzieci
