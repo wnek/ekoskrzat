8e13e65f17ab14512e645eb02e160646f571db0e
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-09-27 14:52:27 +0200

    Change the database to postgres before deploying

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,31 +1,5 @@
 module.exports = ({ env }) => {
   return {
-    backup: {
-      enabled: true,
-      config: {
-        cronSchedule: "0 9-17 * * *", // At minute 0 past every hour from 9 through 17
-        storageService: "aws-s3",
-        awsAccessKeyId: "<AWS_ACCESS_KEY_ID>",
-        awsSecretAccessKey: "<AWS_SECRET_ACCESS_KEY>",
-        sqlite3Executable: "/path/to/your/sqlite3/bin",
-        awsRegion: "<AWS_REGION>",
-        awsS3Bucket: "<AWS_S3_BUCKET>",
-        databaseDriver: env("DATABASE_CLIENT"),
-        mysqldumpExecutable: "/path/to/your/mysqldump/bin",
-        mysqldumpOptions: [
-          "--add-drop-table",
-          "--extended-insert",
-          "--lock-tables",
-          "--dump-date",
-        ],
-        allowCleanup: true,
-        timeToKeepBackupsInSeconds: 172800, // 2 days
-        cleanupCronSchedule: "0 9 * * *", // Each day at 09:00 AM
-        errorHandler: (error, strapi) => {
-          console.log(error);
-        },
-      },
-    },
     email: {
       config: {
         provider: "sendgrid",
