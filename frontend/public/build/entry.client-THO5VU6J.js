25b80204606468f8d839e7d503907d36df1afe0a
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-10-08 11:31:10 +0200

    Change build options

---
+++ frontend/public/build/entry.client-THO5VU6J.js
@@ -0,0 +1,1 @@
+import{a as _,h as i}from"/build/_shared/chunk-2XEKT4MS.js";import{c as y,d as t,e as E,f as n}from"/build/_shared/chunk-VG7UYV5E.js";var c=y(r=>{"use strict";var a=_();r.createRoot=a.createRoot,r.hydrateRoot=a.hydrateRoot;var m});var o=t(E(),1),s=t(c(),1),e=t(n(),1);(0,o.startTransition)(()=>{(0,s.hydrateRoot)(document,(0,e.jsx)(o.StrictMode,{children:(0,e.jsx)(i,{})}))});
