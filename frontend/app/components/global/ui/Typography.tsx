248b6ffcd17b77d2ae585a856762adba64ae7445
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-10-09 22:02:50 +0200

    Add h3 to typography

--- frontend/app/components/global/ui/Typography.tsx
+++ frontend/app/components/global/ui/Typography.tsx
@@ -10,3 +10,9 @@ export function H2({ children }: { children: React.ReactNode }) {
         <h2 className="font-display ~text-[2rem]/[4.5rem] ~leading-[2rem]/[4rem] text-slate-800">{children}</h2>
     )
 }
+
+export function H3({ children }: { children: React.ReactNode }) {
+    return (
+        <h3 className="font-display ~text-[2rem]/[3rem] ~leading-[2rem]/[3rem] text-slate-800">{children}</h3>
+    )
+}
--- frontend/app/components/homepage/Staff.tsx
+++ frontend/app/components/homepage/Staff.tsx
@@ -4,6 +4,8 @@ import {
   useTransform,
 } from "framer-motion";
 
+import { H3 } from "../global/ui/Typography";
+
 import { useRef } from "react";
 
 export default function Staff() {
@@ -28,9 +30,9 @@ export default function Staff() {
 
       </div>
       <div className="flex w-full flex-col items-center gap-8 px-12 py-16 lg:items-start 2xl:px-40">
-        <h2 className="text-center font-display text-2xl text-slate-800 md:text-4xl lg:text-left 2xl:text-6xl">
+        <H3>
           <strong>Wykwalifikowana</strong> kadra
-        </h2>
+        </H3>
         <p className="text-pretty text-center text-slate-600 lg:text-left 2xl:text-2xl">
           Nasi nauczyciele posiadają nie tylko odpowiednie wykształcenie, ale także wykazują ogromne zaangażowanie w pracę z dziećmi. Regularnie uczestniczą w szkoleniach, aby wprowadzać najnowsze metody edukacyjne i wspierać indywidualny rozwój każdego przedszkolaka.”
         </p>
