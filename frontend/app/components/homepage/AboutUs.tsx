3d9b590eb7a5dad29c286b878d72d2614f200492
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-10-16 20:09:04 +0200

    Fix h2 not showing content

--- frontend/app/components/homepage/AboutUs.tsx
+++ frontend/app/components/homepage/AboutUs.tsx
@@ -7,8 +7,8 @@ export default function AboutUs() {
         <h3 className="w-fit rounded-full border border-slate-300 px-4 py-2 text-blue-600">
           O nas
         </h3>
-        <H2>
-          Ekoskrzat to przedszkole w domu z <strong>dużym ogrodem sensoryczno-edukacyjnym i dwoma placami zabaw</strong>, mieszczące się w dzielnicy Bieżanów – Prokocim
+        <H2 html={"Ekoskrzat to przedszkole w domu z <strong>dużym ogrodem sensoryczno-edukacyjnym i dwoma placami zabaw</strong>, mieszczące się w dzielnicy Bieżanów – Prokocim"}>
+
         </H2>
         <p className="max-w-prose text-slate-600 2xl:text-2xl">
           Tutaj dzieci mogą czuć się szczęśliwe, spokojne i bezpieczne, a poprzez kontakt z przyrodą i przebywanie z dala od miejskiego zgiełku mają możliwość poznania wartości, które niesie życie w zdrowym otoczeniu.
