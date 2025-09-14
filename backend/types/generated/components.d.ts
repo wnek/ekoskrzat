85e75134525bb9c9fb6cc722206d480aab61d793
Mariusz Wnęk <wnek.mariusz@gmail.com>
2025-09-14 13:09:34 +0200

    Upgrade strapi to the latest version

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,105 +1,55 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SeoSeo extends Struct.ComponentSchema {
-  collectionName: 'components_seo_seos';
+export interface AboutusOneItem extends Struct.ComponentSchema {
+  collectionName: 'components_aboutus_one_items';
   info: {
-    displayName: 'Seo';
     description: '';
+    displayName: 'OneItem';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String;
-    metaDescription: Schema.Attribute.String;
-    index: Schema.Attribute.Boolean;
-    follow: Schema.Attribute.Boolean;
-  };
-}
-
-export interface HomepageThirdFeature extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_third_features';
-  info: {
-    displayName: 'thirdFeature';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    thirdFeatureImage: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios'
-    >;
-  };
-}
-
-export interface HomepageTestimonials extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_testimonials';
-  info: {
-    displayName: 'testimonials';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    firstTestimonial: Schema.Attribute.Text;
-    firstParents: Schema.Attribute.String;
-    secondTestimonial: Schema.Attribute.Text;
-    secondParents: Schema.Attribute.String;
-    thirdTestimonial: Schema.Attribute.Text;
-    thirdParents: Schema.Attribute.String;
-  };
-}
-
-export interface HomepageStaff extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_staff';
-  info: {
-    displayName: 'staff';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
     description: Schema.Attribute.Text;
-    staffImage: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios'
-    >;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface HomepageSecondFeature extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_second_features';
+export interface HomepageAboutUs extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_about_uses';
   info: {
-    displayName: 'secondFeature';
     description: '';
+    displayName: 'aboutUs';
   };
   attributes: {
-    title: Schema.Attribute.String;
     description: Schema.Attribute.Text;
-    secondFeatureImage: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios'
-    >;
-  };
-}
-
-export interface HomepageNumbers extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_numbers';
-  info: {
-    displayName: 'numbers';
-  };
-  attributes: {
     title: Schema.Attribute.String;
-    firstNumber: Schema.Attribute.Integer;
-    firstLabel: Schema.Attribute.String;
-    secondNumber: Schema.Attribute.Integer;
-    secondLabel: Schema.Attribute.String;
-    thirdNumber: Schema.Attribute.Integer;
-    thirdLabel: Schema.Attribute.String;
-    fourthNumber: Schema.Attribute.Integer;
-    fourthLabel: Schema.Attribute.String;
   };
 }
 
-export interface HomepageMission extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_missions';
+export interface HomepageFirstFeature extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_first_features';
   info: {
-    displayName: 'mission';
     description: '';
+    displayName: 'firstFeature';
   };
   attributes: {
-    text: Schema.Attribute.Text;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageHero extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_heroes';
+  info: {
+    description: '';
+    displayName: 'hero';
+  };
+  attributes: {
+    description: Schema.Attribute.String;
+    image1: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    image2: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    image3: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
   };
 }
 
@@ -109,80 +59,130 @@ export interface HomepageJoin extends Struct.ComponentSchema {
     displayName: 'Join';
   };
   attributes: {
-    title: Schema.Attribute.String;
     description: Schema.Attribute.String;
     joinImage: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface HomepageHero extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_heroes';
+export interface HomepageMission extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_missions';
   info: {
-    displayName: 'hero';
     description: '';
+    displayName: 'mission';
   };
   attributes: {
-    title: Schema.Attribute.String;
-    description: Schema.Attribute.String;
-    image1: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    image2: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    image3: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    text: Schema.Attribute.Text;
   };
 }
 
-export interface HomepageFirstFeature extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_first_features';
+export interface HomepageNumbers extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_numbers';
   info: {
-    displayName: 'firstFeature';
-    description: '';
+    displayName: 'numbers';
   };
   attributes: {
+    firstLabel: Schema.Attribute.String;
+    firstNumber: Schema.Attribute.Integer;
+    fourthLabel: Schema.Attribute.String;
+    fourthNumber: Schema.Attribute.Integer;
+    secondLabel: Schema.Attribute.String;
+    secondNumber: Schema.Attribute.Integer;
+    thirdLabel: Schema.Attribute.String;
+    thirdNumber: Schema.Attribute.Integer;
     title: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageSecondFeature extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_second_features';
+  info: {
+    description: '';
+    displayName: 'secondFeature';
+  };
+  attributes: {
     description: Schema.Attribute.Text;
-    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    secondFeatureImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface HomepageAboutUs extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_about_uses';
+export interface HomepageStaff extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_staff';
   info: {
-    displayName: 'aboutUs';
     description: '';
+    displayName: 'staff';
   };
   attributes: {
-    title: Schema.Attribute.String;
     description: Schema.Attribute.Text;
+    staffImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface AboutusOneItem extends Struct.ComponentSchema {
-  collectionName: 'components_aboutus_one_items';
+export interface HomepageTestimonials extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_testimonials';
   info: {
-    displayName: 'OneItem';
-    description: '';
+    displayName: 'testimonials';
   };
   attributes: {
+    firstParents: Schema.Attribute.String;
+    firstTestimonial: Schema.Attribute.Text;
+    secondParents: Schema.Attribute.String;
+    secondTestimonial: Schema.Attribute.Text;
+    thirdParents: Schema.Attribute.String;
+    thirdTestimonial: Schema.Attribute.Text;
     title: Schema.Attribute.String;
-    description: Schema.Attribute.Text;
-    image: Schema.Attribute.Media<'images' | 'files'>;
+  };
+}
+
+export interface HomepageThirdFeature extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_third_features';
+  info: {
+    description: '';
+    displayName: 'thirdFeature';
+  };
+  attributes: {
+    thirdFeatureImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SeoSeo extends Struct.ComponentSchema {
+  collectionName: 'components_seo_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+  };
+  attributes: {
+    follow: Schema.Attribute.Boolean;
+    index: Schema.Attribute.Boolean;
+    metaDescription: Schema.Attribute.String;
+    metaTitle: Schema.Attribute.String;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'seo.seo': SeoSeo;
-      'homepage.third-feature': HomepageThirdFeature;
-      'homepage.testimonials': HomepageTestimonials;
-      'homepage.staff': HomepageStaff;
-      'homepage.second-feature': HomepageSecondFeature;
-      'homepage.numbers': HomepageNumbers;
-      'homepage.mission': HomepageMission;
-      'homepage.join': HomepageJoin;
-      'homepage.hero': HomepageHero;
-      'homepage.first-feature': HomepageFirstFeature;
-      'homepage.about-us': HomepageAboutUs;
       'aboutus.one-item': AboutusOneItem;
+      'homepage.about-us': HomepageAboutUs;
+      'homepage.first-feature': HomepageFirstFeature;
+      'homepage.hero': HomepageHero;
+      'homepage.join': HomepageJoin;
+      'homepage.mission': HomepageMission;
+      'homepage.numbers': HomepageNumbers;
+      'homepage.second-feature': HomepageSecondFeature;
+      'homepage.staff': HomepageStaff;
+      'homepage.testimonials': HomepageTestimonials;
+      'homepage.third-feature': HomepageThirdFeature;
+      'seo.seo': SeoSeo;
     }
   }
 }
