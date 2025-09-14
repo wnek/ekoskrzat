03a12bca7947519b7b41656fc71475cbadf61fe7
Mariusz Wnęk <wnek.mariusz@gmail.com>
2025-09-14 13:31:06 +0200

    Revert

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -13,10 +13,6 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  // Custom middleware to sanitize repeated folderPath query from Admin Upload plugin
-  {
-    name: "global::sanitize-upload-folderpath",
-  },
   {
     name: "strapi::body",
     config: {
