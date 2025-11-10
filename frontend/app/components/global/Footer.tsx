a8c6020fd5f37f0761da7d9f5d7cceca73e8352e
Mariusz Wnęk <wnek.mariusz@gmail.com>
2025-11-10 21:00:23 +0100

    add google map

--- frontend/app/components/global/Footer.tsx
+++ frontend/app/components/global/Footer.tsx
@@ -4,12 +4,26 @@ export default function Footer() {
       id="footer"
       className="mx-4 flex min-h-screen flex-col items-center gap-12 pt-16 text-center lg:pt-52"
     >
-      <div className="flex w-full flex-col items-center gap-12 lg:flex-row lg:items-start xl:px-32">
-        <div className="w-full xl:w-1/3 2xl:w-1/2">
-          <img src="images/logo.svg" alt="Logo" className="w-8" />
-        </div>
+      <div className="flex w-full flex-col items-center gap-12 sm:flex-row lg:items-stretch xl:px-32">
 
         <div className="flex w-full grid-cols-2 flex-col gap-4 text-left xl:grid">
+
+          <div className="flex flex-col justify-between gap-4 rounded-3xl bg-green-100 p-8 text-slate-800 col-span-2 lg:h-full ">
+            <p className="font-bold 2xl:text-2xl">Mapa dojazdu</p>
+            <iframe
+              title="Mapa dojazdu do Przedszkole Niepubliczne EkoSkrzat Kraków"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4513.91929308413!2d20.032213877784287!3d50.009107671511266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716438e63f52e63%3A0x9002fadc9bd0891f!2sPrzedszkole%20Niepubliczne%20EkoSkrzat%20Krak%C3%B3w!5e1!3m2!1sen!2spl!4v1762804184736!5m2!1sen!2spl"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className="w-full h-[450px] rounded-lg"
+            />
+          </div>
+
+
           <div className="flex flex-col justify-between gap-4 rounded-3xl bg-green-100 p-8 text-slate-800">
             <div className="flex w-fit items-center rounded-full bg-white p-2">
               <img src="images/home.svg" alt="Dom" className="w-6" />
